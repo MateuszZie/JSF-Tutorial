1de4abe1b510decdd26b25cf44c1439408ee61d9
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-02 14:46:49 +0100

    Multi Check Box 2 example.

--- src/main/java/pl/mz/domain/InputText.java
+++ src/main/java/pl/mz/domain/InputText.java
@@ -6,6 +6,9 @@
 package pl.mz.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import lombok.Data;
@@ -20,8 +23,17 @@ import lombok.Data;
 public class InputText {
     private String name;
     private ArrayList<String> names;
+    private Map<String, String> mapedNames;
     
     public String start(){
         return "index";
+    }
+    
+    @PostConstruct
+    public void init(){
+        mapedNames = new HashMap<>();
+        
+        mapedNames.put("C++", "Programing in C++");
+        mapedNames.put("Java", "Programing in Java");
     }
 }
